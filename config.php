f6270494ef3fd55d040d2143a194f62a430ce532
OmondiOchieng <150507369+OmondiOchieng@users.noreply.github.com>
2023-11-11 09:49:12 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // API Auth
 $senderID   = "MOBILESASA";
-$authorization     = "vNfhYSPnFATheb6TNMGrcpEBzz4K7kGcLtkxxtNKrM80X7BsUezlbHNZwQmw";
+$authorization     = "";
 
 // Set the bulkSMSMde flag to 0 so that the subscriber gets charged
 $bulkSMSMode = 0;
